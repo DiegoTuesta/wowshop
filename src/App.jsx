defe92b8a6acec351b5202e2398a42ab94dd2025
Diego Tuesta <diego.productividad@gmail.com>
2023-06-03 19:52:47 -0500

    Maquetado Navbar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <a className="navbar-logo">
+            WOW Shop
+        </a>
+        <div className="navbar-menu">
+          <ul className="navbar-list">
+            <li className="navbar-item"> <a href=""><i className="fa-solid fa-user"></i></a> </li>
+            <li className="navbar-item" > <a href=""> <i className="fa-solid fa-shop"></i></a></li>
+            <li className="navbar-item"> <a href=""><i className="fa-solid fa-cart-shopping"></i></a> </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
