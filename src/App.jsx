c1336ec6a26b8dd5bef97ecd2ae132d5756db234
Diego Tuesta <diego.productividad@gmail.com>
2023-06-05 23:10:57 -0500

    Parte listar Finalizado y login funncionando

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import NewsDetail from "./pages/NewsDetail";
-import Nabvar from "./components/Nabvar";
+import ProductDetail from "./pages/ProductDetail";
+import Navbar from "./components/Nabvar";
 import Loader from "./components/Loader";
 import {useSelector} from "react-redux"
+import Purchases from "./pages/Purchases"
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   const isLoading = useSelector( state => state.loader )
   return (
     <HashRouter>
     { isLoading && < Loader /> }
-    <Nabvar />
+    <Navbar />
     <Routes>
       <Route element={<Home />} path="/" />
       <Route element={<Login />} path="/login" />
-      <Route element={<NewsDetail />} path="/news/:id" />
-     
+      <Route element={ <ProtectedRoutes /> } >
+        <Route element={<Purchases />} path="/purchases" />
+      </Route>
+      <Route element={<ProductDetail />} path="/product/:id" />
+      <Route element={"no se encontro"} path="*" />
     </Routes>
   </HashRouter>
   );
