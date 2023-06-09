70d9d7ab3957f7d0215765681346bdda3ee53659
mario <118136625+Droid-Jr@users.noreply.github.com>
2023-06-09 00:00:10 -0500

    profile data estatica

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -13,7 +13,13 @@ function Profile() {
     return (
     
     <div className="profile">
-        <h1>{user.name}</h1>
+      <div className='profile_data'>
+
+        <h1>DiegoTuesta</h1>
+        <h3>diego@gmail.com</h3>
+        <h4>*******</h4>
+        <h2>987523904</h2>
+      </div>
     </div>
   
   )
