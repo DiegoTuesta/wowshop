eda2f894dd951ce729127445616144821da71c37
mario <118136625+Droid-Jr@users.noreply.github.com>
2023-06-09 00:36:55 -0500

    haciendo dinamico el profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,10 +15,10 @@ function Profile() {
     <div className="profile">
       <div className='profile_data'>
 
-        <h1>DiegoTuesta</h1>
-        <h3>diego@gmail.com</h3>
+        <h1>{user?.firstName} {user?.lastName}</h1>
+        <h3>{user?.email}</h3>
         <h4>*******</h4>
-        <h2>987523904</h2>
+        <h2>{user?.phone}</h2>
       </div>
     </div>
   
